62476eb781834075486d95d3724c3b8bc3ac27dd
XavierIV <mohd.zafranudin@gmail.com>
2022-03-23 01:45:34 +0800

    Feat: Product gallery widget

--- lib/screen/product_view.dart
+++ lib/screen/product_view.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fwt_naelofar_app/model/product.dart';
+import 'package:fwt_naelofar_app/widget/product_gallery.dart';
 
-class ProductView extends StatelessWidget {
+class ProductView extends StatefulWidget {
   const ProductView({Key? key, required this.product}) : super(key: key);
 
   final Product product;
+
+  @override
+  State<ProductView> createState() => _ProductView();
+}
+
+class _ProductView extends State<ProductView> {
+  int _imageIndex = 0;
+
+  setImage(index) {
+    setState(() {
+      _imageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,74 +44,8 @@ class ProductView extends StatelessWidget {
           children: [
             SizedBox(
               height: 300,
-              child: Flex(
-                direction: Axis.horizontal,
-                children: [
-                  Expanded(
-                    flex: 6,
-                    child: Stack(
-                      alignment: AlignmentDirectional.bottomStart,
-                      children: [
-                        SizedBox.expand(
-                          child: Image.network(
-                            product.images[0],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Container(
-                          height: 80.0,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            gradient: LinearGradient(
-                              begin: FractionalOffset.topCenter,
-                              end: FractionalOffset.bottomCenter,
-                              colors: [
-                                Colors.white.withOpacity(0.0),
-                                Colors.white.withOpacity(1.0),
-                              ],
-                              stops: const [0.0, 1.0],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: ListView(
-                      children: const [
-                        SizedBox(
-                          height: 100.0,
-                          width: double.infinity,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(color: Colors.red),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 100.0,
-                          width: double.infinity,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(color: Colors.blue),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 100.0,
-                          width: double.infinity,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(color: Colors.green),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 100.0,
-                          width: double.infinity,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(color: Colors.grey),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+              child: ProductGallery(
+                images: widget.product.images,
               ),
             ),
             Padding(
@@ -106,14 +54,14 @@ class ProductView extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    product.title,
+                    widget.product.title,
                     style: const TextStyle(
                       fontSize: 18.0,
                       fontWeight: FontWeight.w300,
                     ),
                   ),
                   Text(
-                    'RM ${product.price.toStringAsFixed(2)}',
+                    'RM ${widget.product.price.toStringAsFixed(2)}',
                     style: const TextStyle(
                       fontSize: 14.0,
                       fontWeight: FontWeight.w200,
@@ -127,11 +75,14 @@ class ProductView extends StatelessWidget {
                 padding: const EdgeInsets.all(15.0),
                 child: ListView(
                   children: <Widget>[
-                    Text(product.description ?? ''),
+                    Text(widget.product.description ?? ''),
                     const Text('Fabric guide'),
-                    Text('Opacity: ${product.fabricGuide?.opacity?.name}'),
-                    Text('Texture: ${product.fabricGuide?.texture?.name}'),
-                    Text('Thickness: ${product.fabricGuide?.thickness?.name}'),
+                    Text(
+                        'Opacity: ${widget.product.fabricGuide?.opacity?.name}'),
+                    Text(
+                        'Texture: ${widget.product.fabricGuide?.texture?.name}'),
+                    Text(
+                        'Thickness: ${widget.product.fabricGuide?.thickness?.name}'),
                     const Text('Material Care'),
                   ],
                 ),
---
+++ lib/widget/product_gallery.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ProductGallery extends StatefulWidget {
+  const ProductGallery({Key? key, required this.images}) : super(key: key);
+
+  final List<String> images;
+
+  @override
+  State<ProductGallery> createState() => _ProductGallery();
+}
+
+class _ProductGallery extends State<ProductGallery> {
+  int _imageIndex = 0;
+
+  setImage(index) {
+    setState(() {
+      _imageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.horizontal,
+      children: [
+        Expanded(
+          flex: 6,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomStart,
+            children: [
+              SizedBox.expand(
+                child: Image.network(
+                  widget.images[_imageIndex],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: 50.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [
+                      Colors.white.withOpacity(0.0),
+                      Colors.white.withOpacity(1.0),
+                    ],
+                    stops: const [0.0, 1.0],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: ListView.builder(
+              itemCount: widget.images.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () => setImage(index),
+                  child: SizedBox(
+                    height: 120.0,
+                    width: double.infinity,
+                    child: Image.network(
+                      widget.images[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
