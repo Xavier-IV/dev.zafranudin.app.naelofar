4a64a8802c085dada2f1478e955f76ac27904ef7
XavierIV <mohd.zafranudin@gmail.com>
2022-03-23 00:34:47 +0800

    Feat: Added gallery

--- lib/screen/product_view.dart
+++ lib/screen/product_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fwt_naelofar_app/model/product.dart';
 
 class ProductView extends StatelessWidget {
@@ -11,22 +12,93 @@ class ProductView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: ListView(
-          children: <Widget>[
+        child: Column(
+          children: [
             SizedBox(
-              width: 150,
               height: 300,
-              child: Image.network(
-                product.images[0],
+              child: Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomStart,
+                      children: [
+                        SizedBox.expand(
+                          child: Image.network(
+                            product.images[0],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Container(
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            gradient: LinearGradient(
+                              begin: FractionalOffset.topCenter,
+                              end: FractionalOffset.bottomCenter,
+                              colors: [
+                                Colors.white.withOpacity(0.0),
+                                Colors.white.withOpacity(1.0),
+                              ],
+                              stops: const [0.0, 1.0],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: ListView(
+                      children: const [
+                        SizedBox(
+                          height: 100.0,
+                          width: double.infinity,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.red),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100.0,
+                          width: double.infinity,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.blue),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100.0,
+                          width: double.infinity,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.green),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100.0,
+                          width: double.infinity,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.grey),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(product.title),
-            Text(product.description ?? ''),
-            const Text('Fabric guide'),
-            Text('Opacity: ${product.fabricGuide?.opacity?.name}'),
-            Text('Texture: ${product.fabricGuide?.texture?.name}'),
-            Text('Thickness: ${product.fabricGuide?.thickness?.name}'),
-            const Text('Material Care'),
+            Flexible(
+              child: ListView(
+                children: <Widget>[
+                  Text(product.title),
+                  Text(product.description ?? ''),
+                  const Text('Fabric guide'),
+                  Text('Opacity: ${product.fabricGuide?.opacity?.name}'),
+                  Text('Texture: ${product.fabricGuide?.texture?.name}'),
+                  Text('Thickness: ${product.fabricGuide?.thickness?.name}'),
+                  const Text('Material Care'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
